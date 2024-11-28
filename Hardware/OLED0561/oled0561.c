d27f7afd9a59d5f785c949b0bcaab96771af7c66
label01 <2521413036@qq.com>
2024-11-28 16:54:51 +0800

    1. 增加反色接口；2.设计了初步的三个显示菜单

--- Hardware/OLED0561/oled0561.c
+++ Hardware/OLED0561/oled0561.c
@@ -76,17 +76,18 @@ void OLED_DISPLAY_LIT (u8 x){//OLED屏亮度设置（0~255）
 //显示英文与数字8*16的ASCII码
 //取模大小为16*16，取模方式为“从左到右从上到下”“纵向8点下高位”
 void OLED_DISPLAY_8x16(u8 x, //显示汉字的页坐标（从0到7）（此处不可修改）
-						u8 y, //显示汉字的列坐标（从0到63）
+						u8 y, //显示汉字的列坐标（从0到128）
 						u16 w){ //要显示汉字的编号
 	u8 j,t,c=0;
 	y=y+2; //因OLED屏的内置驱动芯片是从0x02列作为屏上最左一列，所以要加上偏移量
 	for(t=0;t<2;t++){
 		I2C_Send_Byte(OLED0561_ADD,COM,0xb0+x); //页地址（从0xB0到0xB7）
-		I2C_Send_Byte(OLED0561_ADD,COM,y/16+0x10); //起始列地址的高4位
+		I2C_Send_Byte(OLED0561_ADD,COM,y/16+0x10); //起始列地址的高4位 高四位第一列固定为0x10， 低四位固定为0x00 
 		I2C_Send_Byte(OLED0561_ADD,COM,y%16);	//起始列地址的低4位
 		for(j=0;j<8;j++){ //整页内容填充
- 			I2C_Send_Byte(OLED0561_ADD,DAT,ASCII_8x16[(w*16)+c-512]);//为了和ASII表对应要减512
-			c++;}x++; //页地址加1
+ 			I2C_Send_Byte(OLED0561_ADD,DAT,ASCII_8x16[(w*16)+c-512]);//为了和ASII表对应要减512   这个计算可以改一改更好理解： （w-32）*16 +c ,asii码第一个字符的十进制表示就是32 ，刷新一列是16位
+			c++;}
+		x++; //页地址加1
 	}
 }
 //向LCM发送一个字符串,长度64字符之内。
@@ -99,17 +100,44 @@ void OLED_DISPLAY_8x16_BUFFER(u8 row,u8 *str){
     }	
 }
 
+
+//反色显示英文字符
+void INVERSE_OLED_DISPLAY_8x16(u8 x, //显示英文页坐标（从0到7）
+																u8 y,//显示英文的列坐标（从0~128）
+																u16 w){//显示的英文编码
+	u8 j, t, c =0;//变量定义
+	y=y+2; //因OLED屏的内置驱动芯片是从0x02列作为屏上最左一列，所以要加上偏移量
+	for(t=0; t<2; t++){
+		I2C_Send_Byte(OLED0561_ADD,COM,0xb0+x); //页地址（从0xB0到0xB7）
+		I2C_Send_Byte(OLED0561_ADD,COM,y/16+0x10); //起始列地址的高4位 高四位第一列固定为0x10， 低四位固定为0x00 
+		I2C_Send_Byte(OLED0561_ADD,COM,y%16);	//起始列地址的低4位
+		for(j=0;j<8;j++){ //整页内容填充
+ 			I2C_Send_Byte(OLED0561_ADD,DAT,~ASCII_8x16[(w*16)+c-512]);//为了和ASII表对应要减512， 按位取反，就可以实现反色效果
+			c++;//记录刷新列
+		}
+		x++; //页地址加1
+	}
+
+
+}
+
+
+
+
+
+
+
 /******************************汉字显示程序***********************************************/
 
 //显示汉字16*16
 //取模大小为16*16，取模方式为“从左到右从上到下”“纵向8点下高位”
 void OLED_DISPLAY_16x16(u8 x, //显示汉字的页坐标（从0xB0到0xB7）
-			u8 y, //显示汉字的列坐标（从0到63）
+			u8 y, //显示汉字的列坐标（从0到128）
 			u16 w){ //要显示汉字的编号
 	u8 j,t,c=0;
 	for(t=0;t<2;t++){
 		I2C_Send_Byte(OLED0561_ADD,COM,0xb0+x); //页地址（从0xB0到0xB7）
-		I2C_Send_Byte(OLED0561_ADD,COM,y/16+0x10); //起始列地址的高4位
+		I2C_Send_Byte(OLED0561_ADD,COM,y/16+0x10); //起始列地址的高4位 ，高四位第一列固定为0x10， 低四位固定为0x00 
 		I2C_Send_Byte(OLED0561_ADD,COM,y%16);	//起始列地址的低4位
 		for(j=0;j<16;j++){ //整页内容填充
  			I2C_Send_Byte(OLED0561_ADD,DAT,GB_16[(w*32)+c]);
@@ -117,6 +145,26 @@ void OLED_DISPLAY_16x16(u8 x, //显示汉字的页坐标（从0xB0到0xB7）
 	}
 	I2C_Send_Byte(OLED0561_ADD,COM,0xAF); //开显示 
 }
+
+//汉字 反色显示
+void INVERSE_OLED_DISPLAY_16x16(u8 x, //显示汉字的页坐标（从0xB0到0xB7）
+			u8 y, //显示汉字的列坐标（从0到128）
+			u16 w){ //要显示汉字的编号
+	u8 j,t,c=0;//变量定义
+	for(t=0;t<2;t++){
+		I2C_Send_Byte(OLED0561_ADD,COM,0xb0+x); //页地址（从0xB0到0xB7）
+		I2C_Send_Byte(OLED0561_ADD,COM,y/16+0x10); //起始列地址的高4位 ，高四位第一列固定为0x10， 低四位固定为0x00 
+		I2C_Send_Byte(OLED0561_ADD,COM,y%16);	//起始列地址的低4位
+		for(j=0;j<16;j++){ //整页内容填充
+ 			I2C_Send_Byte(OLED0561_ADD,DAT,~GB_16[(w*32)+c]);
+			c++;//记录刷新到第几列
+		}
+		x++; //页地址加1
+	}
+	I2C_Send_Byte(OLED0561_ADD,COM,0xAF); //开显示 
+}
+
+
 
 void OLED_DISPLAY_PIC1(void){ //显示全屏图片
 	u8 m,i;
--- Hardware/OLED0561/oled0561.h
+++ Hardware/OLED0561/oled0561.h
@@ -19,11 +19,13 @@ void OLED_DISPLAY_OFF (void);//OLED屏关显示
 void OLED_DISPLAY_LIT (u8 x);//OLED屏亮度设置（0~255）
 void OLED_DISPLAY_CLEAR(void);//清屏操作
 void OLED_DISPLAY_8x16(u8 x,u8 y,u16 w);//显示8x16的单个字符 
+void INVERSE_OLED_DISPLAY_8x16(u8 x, u8 y, u16 w);//反色显示英文字符
 void OLED_DISPLAY_8x16_BUFFER(u8 row,u8 *str);//显示8x16的字符串
 
-void OLED_DISPLAY_16x16(u8 x, u8 y, u16 w);
+void OLED_DISPLAY_16x16(u8 x, u8 y, u16 w); //汉字显示
+void INVERSE_OLED_DISPLAY_16x16(u8 x, u8 y, u16 w); //汉字 反色显示
 
-void OLED_DISPLAY_PIC1(void);
+void OLED_DISPLAY_PIC1(void);//显示图片
 
 
 
