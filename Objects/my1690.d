4a1947848d53f987809f73ceeb6843e14e179cce
label01 <2521413036@qq.com>
2025-01-07 11:38:50 +0800

    添加MY1690的驱动程序，打开串口3 通过编译

---
+++ Objects/my1690.d
@@ -0,0 +1,38 @@
+.\objects\my1690.o: Hardware\MY1690\my1690.c
+.\objects\my1690.o: Hardware\MY1690\my1690.h
+.\objects\my1690.o: .\Basic\sys\sys.h
+.\objects\my1690.o: .\CMSIS\stm32f10x.h
+.\objects\my1690.o: .\CMSIS\core_cm3.h
+.\objects\my1690.o: D:\project software\keil5\ARM\ARMCC\Bin\..\include\stdint.h
+.\objects\my1690.o: .\CMSIS\system_stm32f10x.h
+.\objects\my1690.o: .\User\stm32f10x_conf.h
+.\objects\my1690.o: .\Lib\inc\stm32f10x_adc.h
+.\objects\my1690.o: .\CMSIS\stm32f10x.h
+.\objects\my1690.o: .\Lib\inc\stm32f10x_bkp.h
+.\objects\my1690.o: .\Lib\inc\stm32f10x_can.h
+.\objects\my1690.o: .\Lib\inc\stm32f10x_cec.h
+.\objects\my1690.o: .\Lib\inc\stm32f10x_crc.h
+.\objects\my1690.o: .\Lib\inc\stm32f10x_dac.h
+.\objects\my1690.o: .\Lib\inc\stm32f10x_dbgmcu.h
+.\objects\my1690.o: .\Lib\inc\stm32f10x_dma.h
+.\objects\my1690.o: .\Lib\inc\stm32f10x_exti.h
+.\objects\my1690.o: .\Lib\inc\stm32f10x_flash.h
+.\objects\my1690.o: .\Lib\inc\stm32f10x_fsmc.h
+.\objects\my1690.o: .\Lib\inc\stm32f10x_gpio.h
+.\objects\my1690.o: .\Lib\inc\stm32f10x_i2c.h
+.\objects\my1690.o: .\Lib\inc\stm32f10x_iwdg.h
+.\objects\my1690.o: .\Lib\inc\stm32f10x_pwr.h
+.\objects\my1690.o: .\Lib\inc\stm32f10x_rcc.h
+.\objects\my1690.o: .\Lib\inc\stm32f10x_rtc.h
+.\objects\my1690.o: .\Lib\inc\stm32f10x_sdio.h
+.\objects\my1690.o: .\Lib\inc\stm32f10x_spi.h
+.\objects\my1690.o: .\Lib\inc\stm32f10x_tim.h
+.\objects\my1690.o: .\Lib\inc\stm32f10x_usart.h
+.\objects\my1690.o: .\Lib\inc\stm32f10x_wwdg.h
+.\objects\my1690.o: .\Lib\inc\misc.h
+.\objects\my1690.o: .\Basic\usart\usart.h
+.\objects\my1690.o: .\Basic\delay\delay.h
+.\objects\my1690.o: D:\project software\keil5\ARM\ARMCC\Bin\..\include\stdio.h
+.\objects\my1690.o: D:\project software\keil5\ARM\ARMCC\Bin\..\include\stdarg.h
+.\objects\my1690.o: D:\project software\keil5\ARM\ARMCC\Bin\..\include\stdlib.h
+.\objects\my1690.o: D:\project software\keil5\ARM\ARMCC\Bin\..\include\string.h
